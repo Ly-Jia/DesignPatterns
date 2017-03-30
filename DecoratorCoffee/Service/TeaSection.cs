c9a7b30780b1eb732b06906010609395c28574c2
Ly-Jia Goldstein <hello@lyjia.net>
2017-03-30 23:12:20 +0200

    Cours 31/03

--- DecoratorCoffee/Service/TeaSection.cs
+++ DecoratorCoffee/Service/TeaSection.cs
@@ -1,1 +1,33 @@
+﻿using System;
+using DecoratorCoffee.Domain;
+
+namespace DecoratorCoffee.Service
+{
+    public class TeaSection
+    {
+        public Beverage[] Items { get; private set; }
+        private readonly int MAX_ITEMS = 4;
+        private int numberOfItems;
+
+        public TeaSection()
+        {
+            Items = new Beverage[MAX_ITEMS];
+            Items[0] = new EarlGrey();
             Items[1] = new IcedTea();
+            numberOfItems = 2;
+        }
+
+        public void AddItem(Beverage beverage)
+        {
+            if (numberOfItems >= MAX_ITEMS)
+            {
+                Console.WriteLine("Sorry, section is already full!");
+            }
+            else
+            {
+                Items[numberOfItems] = beverage;
+                numberOfItems += 1;
+            }
+        }
+    }
+}
